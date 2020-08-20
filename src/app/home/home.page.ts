0923d1b66ca89ff3cece23541911b71f89a1cb24
sarlury <jemy.sarlury@gmail.com>
2020-08-20 19:37:52 +0700

    prepared url

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  data: string;
+
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.data = '';
+  }
+
+  ionViewDidEnter(){
+   this.prepareData().subscribe(res => {
+     this.data = JSON.stringify(res);
+   })
+  }
+
+  private prepareData(): Observable<object> {
+    const urlApi = 'https://api.nigelbpeck.com/';
+    return this.http.get(urlApi);
+  }
 
 }
