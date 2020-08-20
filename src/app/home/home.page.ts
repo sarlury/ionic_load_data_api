353c34049e94d272a0c87615494d3fffb5a27c4e
sarlury <jemy.sarlury@gmail.com>
2020-08-20 19:46:33 +0700

    handling error and add loading

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,23 +11,34 @@ import { Observable } from 'rxjs';
 export class HomePage {
 
   data: string;
-
+  error: string;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    public loadCtrl: LoadingController
   ) {
     this.data = '';
+    this.error = '';
   }
 
-  ionViewDidEnter(){
+  async ionViewWillEnter(){
    this.prepareData().subscribe(res => {
      this.data = JSON.stringify(res);
+   }, err => {
+     this.error = `An error occured: ${err.status}, ${err.statusText}`;
    })
   }
 
   private prepareData(): Observable<object> {
     const urlApi = 'https://api.nigelbpeck.com/';
     return this.http.get(urlApi);
+  }
+
+  async presentLoading() {
+    const loading = await this.loadCtrl.create({
+      message: "loading..."
+    });
+    loading.present();
   }
 
 }
